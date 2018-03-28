7bfb427052129342d3694ad2d39f6efbd6ac3c32
Konstantinos Angeletopoulos <kostasAngels@outlook.com>
2018-03-28 17:38:12 +0300

    admin master page setup - gulp installed

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <p>Admin Users Index</p>
+        </div>
+    </div>
+@endsection
